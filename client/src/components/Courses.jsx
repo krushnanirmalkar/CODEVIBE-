06eb8ee769f51ef352a5b28d8fea72211e8174c0
Your GitHub Name <youremail@example.com>
2026-05-15 19:39:48 +0530

    fix(client): improve course card alignment

--- client/src/components/Courses.jsx
+++ client/src/components/Courses.jsx
@@ -19,26 +19,23 @@ const Courses = () => {
       <h2>Available Courses</h2>
       <div className='course-name'>
 
-        <div className="course-box">
-          <img src={htmlLogo} alt="HTML Logo" height="300px" width="200px" />
+        <Link to="/HtmlLesson" className="course-box">
+          <img src={htmlLogo} alt="HTML Logo"/>
           <h3>HTML Basics</h3>
           <p>Start your web development journey with HTML.</p>
-          <Link to="/HtmlLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={cssLogo} alt="CSS Logo" height="300px" width="200px" />
+        <Link to="/CssLesson" className="course-box">
+          <img src={cssLogo} alt="CSS Logo"/>
           <h3>CSS for Beginner</h3>
           <p>Learn how to style beautiful websites.</p>
-          <Link to="/CssLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={jsLogo} alt="JavaScript Logo" height="300px" width="200px"/>
+        <Link to="/JsLesson" className="course-box">
+          <img src={jsLogo} alt="JavaScript Logo"/>
           <h3>JS for Beginner</h3>
           <p>Learn how to give functionality to websites.</p>
-          <Link to="/JsLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
          {/* <div className="course-box">
           <img src={cLogo} alt="C Logo" height="300px" width="200px" />
@@ -47,47 +44,45 @@ const Courses = () => {
           <Link to="/CLesson">Start Lesson</Link>
         </div>  */}
 
-        <div className="course-box">
-          <img src={OOPLogo} alt="OOP Logo" height="300px" width="200px" />
+        <Link to="/OopLesson" className="course-box">
+          <img src={OOPLogo} alt="OOP Logo" />
           <h3>OOP Concepts</h3>
           <p>“Think in objects, not just code.” Learn how real-world programming works.</p>
-          <Link to="/OopLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={dsaLogo} alt="DSA Logo" height="300px" width="200px" />
+        <Link to="/DsaLesson" className="course-box">
+          <img src={dsaLogo} alt="DSA Logo"/>
           <h3>Data Structures & Algorithms</h3>
           <p>“Code faster, run smarter.” Build the backbone of efficient programming.</p>
-          <Link to="/DsaLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={nodeLogo} alt="Node.js Logo" height="300px" width="200px" />
+        <Link to="/NodeLesson" className="course-box">
+          <img src={nodeLogo} alt="Node.js Logo"/>
           <h3>Node.js</h3>
           <p>“JavaScript, but on steroids.” Learn backend development with ease.</p>
-          <Link to="/NodeLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={reactLogo} alt="React Logo" height="300px" width="200px" />
+        <Link to="/ReactLesson" className="course-box">
+          <img src={reactLogo} alt="React Logo" />
           <h3>React.js</h3>
           <p>“Build once, render everywhere.” Master the king of frontend frameworks.</p>
-          <Link to="/ReactLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={expressLogo} alt="Express Logo" height="300px" width="200px" />
+        <Link to="/ExpressLesson" className="course-box" >
+          <img src={expressLogo} alt="Express Logo"style={{
+          objectFit: "fill",
+          height: "120px",
+          marginTop: "40px"
+        }}/>
           <h3>Express.js</h3>
           <p>“Backend, but lightning fast.” Simplify server-side development.</p>
-          <Link to="/ExpressLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
-        <div className="course-box">
-          <img src={mongoLogo} alt="MongoDB Logo" height="300px" width="200px" />
+        <Link to="/MongoLesson" className="course-box">
+          <img src={mongoLogo} alt="MongoDB Logo" />
           <h3>MongoDB</h3>
           <p>“Store data like a pro.” Learn the NoSQL database of the modern web.</p>
-          <Link to="/MongoLesson">Start Lesson</Link>
-        </div>
+        </Link>
 
       </div>
     </div>
